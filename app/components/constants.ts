4b6f70d1ae4ec799e9b3d315e4731147e27eb3f3
sktbrd <sktbrd.eth@gmail.com>
2025-07-23 09:00:02 -0300

    feat: Remove unused BuyTransactionSimple, ArrowSvg, and OnchainKit components

--- app/components/constants.ts
+++ app/components/constants.ts
@@ -3,19 +3,3 @@ export const ACCOUNT_PRICE_ETH = '0.0000001'; // Price in ETH for account creati
 
 export const RECIPIENT_ADDRESS = '0xB4964e1ecA55Db36a94e8aeFfBFBAb48529a2f6c' as const; // Skatehive hot wallet address 
 
-export const components = [
-  {
-    name: 'Transaction',
-    url: 'https://onchainkit.xyz/transaction/transaction',
-  },
-  { name: 'Swap', url: 'https://osnchainkit.xyz/swap/swap' },
-  { name: 'Checkout', url: 'https://onchainkit.xyz/checkout/checkout' },
-  { name: 'Wallet', url: 'https://onchainkit.xyz/wallet/wallet' },
-  { name: 'Identity', url: 'https://onchainkit.xyz/identity/identity' },
-];
-
-export const templates = [
-  { name: 'NFT', url: 'https://github.com/coinbase/onchain-app-template' },
-  { name: 'Commerce', url: 'https://github.com/coinbase/onchain-commerce-template'},
-  { name: 'Fund', url: 'https://github.com/fakepixels/fund-component' },
-];
